c29ddeb61345b123a0c0a05ac427128b4ca4ab31
PinoRIP <ItsPinoRIP@gmail.com>
2023-07-29 17:02:39 +0200

    Initial commit

---
+++ Source/FunkProject/FunkProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FunkProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUNKPROJECT_API AFunkProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
